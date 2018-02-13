344db310fdaebf508b360dd8aa58c8a1216be390
Ainsey11 <robert@ainsey11.com>
2018-02-13 23:05:01 +0000

    generic loop configuration of pins
    this method actually works, adding a lot of comments so I can backtrack
    what I did and why for now

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,16 +1,28 @@
+int counter = 0;
+int ModeUpPin = 1; //Pin goes HIGH to increase mode
+int ModeDownPin = 2; //Pin goes LOW to decrease mode
+int Mode; //Actual counter variable
+int LEDPins[] = { 5, 6, 7, 8, 9, 10, 11, 12}; // Pins to use to drive LED circuits
+
 void setup() {
-  // initialize pins as output
+  pinMode (ModeUpPin, INPUT);
+  pinMode (ModeDownPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT); //Sets onboard LED as output (handy for testing)
-  int inMin = 5; //Lowest output pin
-  int inMax = 12 // Highest output pin
-  for(int i=inMin; i<=inMax; i++)
+  
+  for(int i=0;i<8;i++) // 8 used here as 8 pins in the array
   {
-    pinMode(i, INPUT);
+    pinMode(LEDPins[i], OUTPUT); //declaring LED's as outputs
   }
 }
+
+// Mode select section
+
+
+
 
 // the loop function runs over and over again forever
 void loop() {
 }
 
-
+//Notes
+// https://github.com/pkarsy/NonVolatile - for persistant mode selection storage maybe?
