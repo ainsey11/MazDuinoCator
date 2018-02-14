d699f5603d3da1b3fd2f38a3ba97ef225afb60af
Ainsey11 <robert@ainsey11.com>
2018-02-14 00:35:04 +0000

    Commenting out, don't think this is needed anymore

--- Main/Main.ino
+++ Main/Main.ino
@@ -9,17 +9,17 @@ int ModeDownPin = 2; //Pin goes LOW to decrease mode
 int LeftIndicatorPin = 3; // Pin goes HIGH to trigger left indicator sequence
 int RightIndicatorPin = 4; // Pin goes HIGH to trigger right indicator sequence
 
-int LEDPins[] = { 5, 6, 7, 8, 9, 10, 11, 12}; // Pins to use to drive LED circuits - Might not be needed when I have the shift register all done and in
+//int LEDPins[] = { 5, 6, 7, 8, 9, 10, 11, 12}; // Pins to use to drive LED circuits - Might not be needed when I have the shift register all done and in
 
 void setup() {
   pinMode (ModeUpPin, INPUT);
   pinMode (ModeDownPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT); //Sets onboard LED as output (handy for testing)
   
-  for(int i=0;i<8;i++) // 8 used here as 8 pins in the array
-  {
-    pinMode(LEDPins[i], OUTPUT); //declaring LED's as outputs
-  }
+  //for(int i=0;i<8;i++) // 8 used here as 8 pins in the array
+  //{
+   // pinMode(LEDPins[i], OUTPUT); //declaring LED's as outputs
+  //}
 }
 
 // the loop function runs over and over again forever
