9f08abe2dd9bd8c5c37a88502a41bce6d3cd82e3
Ainsey11 <robert@ainsey11.com>
2018-02-16 23:36:24 +0000

    Adding in SR lib stuff and a basic mode switch test
    don't need the indicator/r-l sr pin config for now

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,27 +1,20 @@
 #include "NonVolatile.h"
 #include "EEPROMSave.h"
 #include "testLED.h"
+#include "ShiftRegister74HC595.h"
 
 
 NonVolatile<int> Mode; //Actual counter variable and EEPROM 0-1
-int ModeUpPin = 1; //Pin goes HIGH to increase mode
-int ModeDownPin = 2; //Pin goes LOW to decrease mode
-int LeftIndicatorPin = 3; // Pin goes HIGH to trigger left indicator sequence
-int RightIndicatorPin = 4; // Pin goes HIGH to trigger right indicator sequence
-int RightLatchPin = 5; // connect to ST_CP of R SR
-int RightClockPin = 6; // connect to SH_CP of R SR
-int RightDataPin = 7; // connect to DS of R SR
-int LeftLatchPin = 5; // connect to ST_CP of L SR
-int LeftClockPin = 6; // connect to SH_CP of L SR
-int LeftDataPin = 7; // connect to DS of L SR
-
+int ModeUpPin = 11; //Pin goes HIGH to increase mode
+int ModeDownPin = 12; //Pin goes LOW to decrease mode
+ShiftRegister74HC595 sr (1, 2, 4, 3); 
 
 //Pin connected to ST_CP of 74HC595
-//int latchPin = 3;
+int latchPin = 3;
 //Pin connected to SH_CP of 74HC595
-//int clockPin = 4;
+int clockPin = 4;
 ////Pin connected to DS of 74HC595
-//int dataPin = 2;
+int dataPin = 2;
 
 
 
